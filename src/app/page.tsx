7cbb5d2566999e7a7b810352064d707decd7d580
Anand Raj <anandrajj10@gmail.com>
2026-06-12 03:02:34 +0530

    Tailwind setup added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { Metadata } from "next";
+import { pagesConfig } from "../../config/pages";
 import styles from "./page.module.css";
+import Navigation from "@/app/Navigation/page";
+
+export const metadata: Metadata = {
+  title: pagesConfig.home.metadata.title,
+  description: pagesConfig.home.metadata.description,
+};
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Navigation />
         <h1 className={styles.title}>Welcome Anand !</h1>
       </main>
     </div>
