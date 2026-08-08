6e29e5d4ea17e7f98db71856fe61bb2728642a79
Anand Raj <anandrajj10@gmail.com>
2026-08-09 00:46:45 +0530

    reswitch

--- src/app/components/layout/AppShell.tsx
+++ src/app/components/layout/AppShell.tsx
@@ -1,6 +1,6 @@
-import Navigation from './../../navigation/page';
-import ChatPortal from '../../../app/components/Chat/ChatPortal';
-import AnimatedBackground from '../../../app/components/AnimatedBackground';
+import Navigation from '@/app/navigation/page';
+import ChatPortal from '@/app/components/Chat/ChatPortal';
+import AnimatedBackground from '@/app/components/AnimatedBackground';
 // import Sidebar from './Sidebar';
 
 interface AppShellProps {
